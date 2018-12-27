179086c658cb8b0e22e330b2eb24110b21544512
Fang Yi <yifang@faceunity.com>
2018-12-27 14:42:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,69 @@
+misc
+====
+
+
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/misc.svg)](https://npmjs.org/package/misc)
+[![Downloads/week](https://img.shields.io/npm/dw/misc.svg)](https://npmjs.org/package/misc)
+[![License](https://img.shields.io/npm/l/misc.svg)](https://github.com/fullstackoverflow/Misc-cli/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g misc
+$ misc COMMAND
+running command...
+$ misc (-v|--version|version)
+misc/1.0.0 win32-x64 node-v8.11.4
+$ misc --help [COMMAND]
+USAGE
+  $ misc COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+* [`misc help [COMMAND]`](#misc-help-command)
+* [`misc new [FILE]`](#misc-new-file)
+
+## `misc help [COMMAND]`
+
+display help for misc
+
+```
+USAGE
+  $ misc help [COMMAND]
+
+ARGUMENTS
+  COMMAND  command to show help for
+
+OPTIONS
+  --all  see all commands in CLI
+```
+
+_See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v2.1.4/src\commands\help.ts)_
+
+## `misc new [FILE]`
+
+create new project with misc
+
+```
+USAGE
+  $ misc new [FILE]
+
+OPTIONS
+  -f, --force
+  -h, --help       show CLI help
+  -n, --name=name  name to print
+
+EXAMPLE
+  $ misc new ./
+```
+
+_See code: [src\commands\new.ts](https://github.com/fullstackoverflow/misc-cli/blob/v1.0.0/src\commands\new.ts)_
+<!-- commandsstop -->
---
+++ src/commands/new.ts
@@ -0,0 +1,53 @@
+import { Command, flags } from "@oclif/command";
+import cli from "cli-ux";
+import { statSync, readdirSync, mkdirSync, writeFileSync, readFileSync, existsSync } from "fs";
+import { sync } from "rimraf";
+import { join } from "path";
+import { execSync } from "child_process";
+
+export default class NewProject extends Command {
+  static description = "create new project with misc";
+
+  static examples = [`$ misc new ./`];
+
+  static flags = {
+    help: flags.help({ char: "h" }),
+    // flag with no value (-f, --force)
+    force: flags.boolean({ char: "f" })
+  };
+
+  static args = [{ name: "path" }];
+
+  async run() {
+    const { args, flags } = this.parse(NewProject);
+    const { path } = args;
+    if (path) {
+      if (!flags.force) {
+        if (existsSync(path) && statSync(path).isDirectory()) {
+          if (readdirSync(path).length > 0) {
+            const cover = await cli.prompt("Directory is not empty,cover or not(Y/N)?");
+            if (cover === "N") {
+              return;
+            } else if (cover === "Y") {
+              sync(path);
+              mkdirSync(path);
+            }
+          }
+        } else {
+          mkdirSync(path);
+        }
+      } else {
+        sync(path);
+        mkdirSync(path);
+      }
+      const projectName = await cli.prompt("What is the project name?");
+      execSync("git clone https://github.com/fullstackoverflow/Misc-exmaple.git .", { cwd: path, stdio: "inherit" });
+      sync(join(path, ".git"));
+      const pkg = JSON.parse(readFileSync(join(path, "package.json")).toString());
+      pkg.name = projectName;
+      writeFileSync(join(path, "package.json"), JSON.stringify(pkg, null, 4));
+      execSync("npm i", { cwd: path, stdio: "inherit" });
+      this.log(`project ${projectName} create success`);
+    }
+  }
+}
